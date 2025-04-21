0c0f5272294f4b4b81144792160827e57ec16697
Irvin <118541846+IrvinTM@users.noreply.github.com>
2025-04-21 19:28:38 +0000

    navbar basic setup

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,70 @@
+import { useState } from "react";
+import {X, Menu} from 'lucide-react';
 
 export default function NavBar() {
 
+      // State to manage the navbar's visibility
+  const [nav, setNav] = useState(false);
+
+  // Toggle function to handle the navbar's display
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  // Array containing navigation items
+  const navItems = [
+    { id: 1, text: 'Home' },
+    { id: 2, text: 'Sales' },
+    { id: 3, text: 'Products' },
+    { id: 4, text: 'Settings' },
+  ];
+
     return (
-        <div>
-        <h1 className="text-gre">Hello</h1>
-        </div>
+        <>
+        <div className='flex justify-between items-center h-12 max-w-[1240px] mx-auto px-4'>
+      {/* Logo */}
+      <h1 className='w-full text-3xl font-bold text-primary-light'>REACT.</h1>
+
+      {/* Desktop Navigation */}
+      <ul className='hidden md:flex'>
+        {navItems.map(item => (
+          <li
+            key={item.id}
+            className='m-4 p-4 text-sm/6 font-semibold text-white/50 focus:outline-none  cursor-pointer'
+          >
+            {item.text}
+          </li>
+        ))}
+      </ul>
+
+      {/* Mobile Navigation Icon */}
+      <div onClick={handleNav} className='block md:hidden'>
+        {nav ? <X /> :  <Menu />}
+      </div>
+
+      {/* Mobile Navigation Menu */}
+      <ul
+        className={
+          nav
+            ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'
+            : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
+        }
+      >
+        {/* Mobile Logo */}
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
+
+        {/* Mobile Navigation Items */}
+        {navItems.map(item => (
+          <li
+            key={item.id}
+            className='p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600'
+          >
+            {item.text}
+          </li>
+        ))}
+      </ul>
+    </div>
+        
+        </>
     )
 }
