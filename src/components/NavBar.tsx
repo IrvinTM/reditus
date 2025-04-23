d66f6e4bd7ed7e4211199003bc00ee19222aab81
IrvinTM <irvintm@gh.com>
2025-04-23 18:18:05 +0000

    changed layout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
-import  React, { useState, ReactNode} from "react";
-import {X, Menu} from 'lucide-react';
+import  React, { useState, ReactNode, useEffect} from "react";
+import {X, Menu, Home, Store, Package, Settings} from 'lucide-react';
 
 interface NavBarProps {
   home: ReactNode
@@ -27,6 +27,10 @@ const NavBar: React.FC<NavBarProps> = ({home, sales, products, settings}: NavBar
     setNav(!nav);
   };
 
+  const closeNav = () => {
+    setNav(false);
+  };
+
   // Array containing navigation items
   const navItems = [
     { id: 1, text: 'Home' },
@@ -35,50 +39,64 @@ const NavBar: React.FC<NavBarProps> = ({home, sales, products, settings}: NavBar
     { id: 4, text: 'Settings' },
   ];
 
+  useEffect(() => {
+    setSelectedItem("Home");
+  }, []);
+
     return (
         <>
-        <div className='flex justify-between items-center h-12 mx-auto px-4 bg-base'>
-      {/* Logo */}
-      <h1 className='w-full text-3xl font-bold text-primary-dark'>REACT.</h1>
+        <div className='fixed right-0 top-0 h-full shadow-xl shadow-surface-0 p-2 rounded-2xl'>
 
-      {/* Desktop Navigation */}
-      <ul className='hidden md:flex'>
-        {navItems.map(item => (
-          <li
-            key={item.id}
-            className={`m-4 px-4 py-1 text-sm/6 font-semibold  focus:outline-none hover:text-primary-dark  cursor-pointer ${selectedItem === item.text ? 'rounded-xl shadow-surface-2 bg-gradient-to-t from-surface-0 to-base text-primary-dark' : 'bg-base text-white/50'}`}
-            onClick={() => setSelectedItem(item.text)}
-          >
-            {item.text}
-          </li>
-        ))}
-      </ul>
 
       {/* Mobile Navigation Icon */}
-      <div onClick={handleNav} className='block md:hidden '>
-        {nav ? <X /> :  <Menu />}
+     <div className="flex flex-col justify-center items-center text-center">
+     <div className="flex flex-col justify-center items-center text-center" onClick={handleNav}>
+        {nav ? <X className="w-12 h-12 mt-4 text-text" /> :  <Menu className="w-12 h-12 mt-4 text-text" />}
       </div>
+      <div className={`flex flex-col justify-center items-center text-center w-full ${selectedItem === "Home" ? 'rounded-xl shadow-surface-2 shadow-sm bg-gradient-to-t from-surface-2 to-surface-0 text-primary-dark' : 'bg-base text-white/50'}`} onClick={()=>{setSelectedItem("Home")}}>
+        { <Home className="w-12 h-12 mt-4 text-text" /> }
+        <p>Home</p>
+      </div>
+      <div className={`flex flex-col justify-center items-center text-center w-full ${selectedItem === "Sales" ? 'rounded-xl shadow-surface-2 shadow-sm bg-gradient-to-t from-surface-2 to-surface-0 text-primary-dark' : 'bg-base text-white/50'}`} onClick={()=>{setSelectedItem("Sales")}}>
+        { <Store className="w-12 h-12 mt-4 text-text" /> }
+        <p >Sales</p>
+      </div>
+      <div className={`flex flex-col justify-center items-center text-center w-full ${selectedItem === "Products" ? 'rounded-xl shadow-surface-2 shadow-sm bg-gradient-to-t from-surface-2 to-surface-0 text-primary-dark' : 'bg-base text-white/50'}`} onClick={()=>{setSelectedItem("Products")}}>
+        { <Package className="w-12 h-12 mt-4 text-text" /> }
+        <p className="m-1">Products</p>
+      </div>
+      <div className={`flex flex-col justify-center items-center text-center w-full ${selectedItem === "Settings" ? 'rounded-xl shadow-surface-2 shadow-sm bg-gradient-to-t from-surface-2 to-surface-0 text-primary-dark' : 'bg-base text-white/50'}`} onClick={()=>{setSelectedItem("Settings")}}>
+        { <Settings className="w-12 h-12 mt-4 text-text" /> }
+        <p className="m-1">Settings</p>
+      </div>
+     </div>
+
 
       {/* Mobile Navigation Menu */}
       <ul
         className={
           nav
-            ? 'fixed md:hidden left-0 top-0 w-[60%] h-full ease-in-out duration-500 text-sm/6 font-semibold text-white/50 focus:outline-none hover:text-primary-dark  cursor-pointer bg-base '
-            : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%] bg-base'
+            ? 'fixed shadow-2xl shadow-surface-0 right-0 top-0 w-auto rounded-sm h-full ease-in-out duration-500 text-sm/6 font-semibold text-white/50 focus:outline-none hover:text-primary-dark  cursor-pointer bg-base '
+            : 'ease-in-out w-auto duration-500 fixed top-0 bottom-0 right-[-100%]'
         }
       >
-        {/* Mobile Logo */}
-        <h1 className='w-full text-3xl font-bold text-primary-dark'>REACT.</h1>
 
-        {/* Mobile Navigation Items */}
-        {navItems.map(item => (
-          <li
-            key={item.id}
-            className='p-4 rounded-xl  cursor-pointer border-surface-0 text-sm/6 font-semibold text-text focus:outline-none hover:text-primary-hover'
-          >
-            {item.text}
-          </li>
+
+<div className="flex flex-col items-center mt-4">
+<div onClick={handleNav}>
+        {nav ? <X className="w-12 h-12 text-text mt-4" /> :  <Menu className="w-6 h-6 text-text mt-4" />}
+      </div>
+{/* Mobile Navigation Items */}
+{navItems.map(item => (
+           <li
+           key={item.id}
+           className={`m-12 p-12 py-1 text-xl font-semibold  focus:outline-none hover:text-primary-dark  cursor-pointer ${selectedItem === item.text ? 'rounded-xl shadow-surface-2 shadow-sm bg-gradient-to-t from-surface-0 to-base text-primary-dark' : 'bg-base text-white/50'}`}
+           onClick={() => {setSelectedItem(item.text); handleNav();}}
+         >
+           {item.text}
+         </li>
         ))}
+      </div>
       </ul>
     </div>
     <div>
@@ -86,8 +104,6 @@ const NavBar: React.FC<NavBarProps> = ({home, sales, products, settings}: NavBar
         selectedItem ? optionsMap.get(selectedItem) : home
       }
     </div>
-
-        
         </>
     )
 }
