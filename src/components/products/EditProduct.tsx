ef5b77cff58cf99aa162e8617b5b1dff0445177f
IrvinTM <irvintm23@gmail.com>
2025-06-30 22:16:42 -0600

    edit product functionallity

--- src/components/products/EditProduct.tsx
+++ src/components/products/EditProduct.tsx
@@ -12,13 +12,35 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Product } from "@/types/types"
+import { ChangeEvent, useState } from "react"
 
 interface editDialog{
   product:Product
-
 }
 
 export function EditDialog({product}: editDialog) {
+  
+    const [prod, setProd] = useState<Product>(product);
+
+    const appUrl = import.meta.env.VITE_BACK_URL
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>)=>{
+      const {name, value} = e.target
+    setProd((prevProduct)=>({
+      ...prevProduct, [name]: value
+    }
+    ))
+  }
+const handleSubmit = ()=>{
+    fetch(appUrl+"/api/products/update", {
+      method: "put",
+      body: JSON.stringify(prod),
+      headers:{
+        "Content-Type": "Application/json"
+      }
+    })
+  }
+  
   return (
     <Dialog>
       <form>
@@ -34,55 +56,58 @@ export function EditDialog({product}: editDialog) {
           </DialogHeader>
           <div className="grid gap-4">
             <div className="grid gap-3">
-              <Label htmlFor="product-name">Nombre</Label>
-              <Input id="product-name" name="product-name" defaultValue={product.name} />
+              <Label htmlFor="name">Nombre</Label>
+              <Input id="name" onChange={handleChange} name="name" defaultValue={prod.name} />
             </div>
             <div className="grid gap-3">
               <Label htmlFor="product-code">Codigo</Label>
-              <Input id="product-code" name="product-code" defaultValue={product.code} />
+              <Input id="product-code" name="code" defaultValue={prod.code} />
             </div>
             <div className="grid gap-3">
               <Label htmlFor="product-barcode">Codigo de barras</Label>
-              <Input id="product-barcode" name="product-barcode" defaultValue={product.barCode} />
+              <Input id="product-barcode" name="barCode" defaultValue={prod.barCode} />
             </div>
             <div className="grid gap-3">
               <Label htmlFor="measurement-unit">Unidad de medida</Label>
-              <Input id="measurement-unit" name="measurement-unit" defaultValue={product.measurementUnit} />
+              <Input id="measurement-unit" name="measurementUnit" defaultValue={prod.measurementUnit} />
             </div>
             <div className="grid gap-3">
               <Label htmlFor="description">Descripción</Label>
-              <Input id="description" name="description" defaultValue={product.description} />
+              <Input id="description" name="description" defaultValue={prod.description} />
             </div>
             <div className="grid gap-3">
               <Label htmlFor="image">Imagen</Label>
-              <Input id="image" name="image" defaultValue={product.image} />
+              <Input id="image" name="image" defaultValue={prod.image} />
             </div>
             <div className="grid gap-3">
               <Label htmlFor="cost">Costo</Label>
-              <Input id="cost" name="cost" defaultValue={product.cost} type="number" />
+              <Input id="cost" name="cost" defaultValue={prod.cost} type="number" />
             </div>
             <div className="grid gap-3">
               <Label htmlFor="no-tax-included-price">Precio sin impuestos</Label>
-              <Input id="no-tax-included-price" name="no-tax-included-price" defaultValue={product.noTaxIncludedPrice} type="number" />
+              <Input id="no-tax-included-price" name="noTaxIncludedPrice" defaultValue={prod.noTaxIncludedPrice} type="number" />
             </div>
             <div className="grid gap-3">
               <Label htmlFor="tax-included-price">Precio con impuestos</Label>
-              <Input id="tax-included-price" name="tax-included-price" defaultValue={product.taxIncludedPrice} type="number" />
+              <Input id="tax-included-price" name="taxIncludedPrice" defaultValue={prod.taxIncludedPrice} type="number" />
             </div>
             <div className="grid gap-3">
               <Label htmlFor="profit-margin">Margen de ganancia</Label>
-              <Input id="profit-margin" name="profit-margin" defaultValue={product.profitMargin} type="number" />
+              <Input id="profit-margin" name="profitMargin" onChange={()=>{handleChange}} defaultValue={prod.profitMargin} type="number" />
             </div>
             <div className="grid gap-3">
               <Label htmlFor="sales-price">Precio de venta</Label>
-              <Input id="sales-price" name="sales-price" defaultValue={product.salesPrice} type="number" />
+              <Input id="sales-price" name="salesPrice" defaultValue={prod.salesPrice} type="number" />
             </div>
           </div>
           <DialogFooter>
             <DialogClose asChild>
               <Button variant="outline">Cancelar</Button>
             </DialogClose>
-            <Button type="submit">Guardar cambios</Button>
+
+            <DialogClose asChild>
+            <Button onClick={()=>handleSubmit()}>Guardar cambios</Button>
+            </DialogClose>
           </DialogFooter>
         </DialogContent>
       </form>
